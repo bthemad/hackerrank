5782416338cacd271e8b40e7e54706f4b0ee2d89
Alexander Kudryashov <alexander.kudryashov@softonic.com>
2015-01-29 18:32:05 +0100

    And one more

---
+++ algo/is_fibo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+
+def fib_generator():
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a
+
+
+def is_fibo(a):
+    i = 0
+    g = fib_generator()
+    while True:
+        i = g.next()
+        if a == i:
+            return True
+        if i > a:
+            return False
+
+
+if __name__ == '__main__':
+    t = input()
+    for i in range(t):
+        a = int(raw_input())
+        if is_fibo(a):
+            print "IsFibo"
+        else:
+            print "IsNotFibo"
