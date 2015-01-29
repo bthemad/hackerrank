a9475c5e1b67fd26254cff17f37c97589e9b09f4
Alexander Kudryashov <alexander.kudryashov@softonic.com>
2015-01-29 18:00:01 +0100

    Now we have a template

---
+++ algo/template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+def fun(a):
+    pass
+
+
+if __name__ == '__main__':
+    # t = input()
+    t = []
+    # for i in range(t):
+    for a in t:
+        # a = map(int, raw_input().strip().split(" "))
+        print fun(a)
